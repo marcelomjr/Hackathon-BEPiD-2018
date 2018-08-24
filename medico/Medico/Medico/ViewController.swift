fc25776c157067a0449504fb924f577aaa47683b
Marcelove <martimiano.marcelo@gmail.com>
2018-08-24 18:44:55 -0300

    criacao de projeto

---
+++ medico/Medico/Medico/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Medico
+//
+//  Created by Marcelo Martimiano Junior on 24/08/2018.
+//  Copyright © 2018 Marcelo Martimiano Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
