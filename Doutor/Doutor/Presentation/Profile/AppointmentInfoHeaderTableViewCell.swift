0440839633a39af1660926e26087a63eeb13aee7
seongeunkim <grah.kim@gmail.com>
2018-08-25 10:09:49 -0300

    appoint info

---
+++ Doutor/Doutor/Presentation/Profile/AppointmentInfoHeaderTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  AppointmentInfoHeaderTableViewCell.swift
+//  Doutor
+//
+//  Created by Seong Eun Kim on 25/08/18.
+//  Copyright © 2018 Seong Eun Kim. All rights reserved.
+//
+
+import UIKit
+
+class AppointmentInfoHeaderTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
