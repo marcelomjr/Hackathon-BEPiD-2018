4aeda2e52d7ef1232467a05e377927cbe6faf529
seongeunkim <grah.kim@gmail.com>
2018-08-25 13:52:27 -0300

    tela de consultas feita

---
+++ usuario/Paciente/Paciente/View/AppointmentsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AppointmentsTableViewCell.swift
+//  Paciente
+//
+//  Created by Seong Eun Kim on 25/08/18.
+//  Copyright © 2018 Marcelo Martimiano Junior. All rights reserved.
+//
+
+import UIKit
+
+class AppointmentsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ usuario/Paciente/Paciente/View/DesignView.swift
@@ -0,0 +1,12 @@
+//
+//  DesignCell.swift
+//  Paciente
+//
+//  Created by Seong Eun Kim on 25/08/18.
+//  Copyright © 2018 Marcelo Martimiano Junior. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
